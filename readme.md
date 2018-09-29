aa52aaa2bb03cad38702b6a106343af84d799a83
lihaoyang <lihaoyang@huayugroup360.com>
2018-09-29 14:54:45 +0800

    init

---
+++ readme.md
@@ -0,0 +1,5 @@
+## ueditor-qiniu-spring-boot-start 使用案例<br>
+* 下载本项目
+* 运行项目
+* 浏览器打开[http://localhost](http://localhost)
+* ueditor-qiniu-spring-boot-start [源码](https://github.com/weiangongsi/ueditor-qiniu-spring-boot-start)
---
+++ src/main/java/com/example/ueditorspringbootstarterexample/UeditorSpringBootStarterExampleApplication.java
@@ -0,0 +1,32 @@
+package com.example.ueditorspringbootstarterexample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class UeditorSpringBootStarterExampleApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(UeditorSpringBootStarterExampleApplication.class);
+    static int port;
+
+    @Value("${server.port}")
+    public void setPort(int port) {
+        UeditorSpringBootStarterExampleApplication.port = port;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UeditorSpringBootStarterExampleApplication.class, args);
+        logger.info("http://localhost:" + port);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "ue";
+    }
+}
