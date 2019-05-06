3030ce350490cc9ec3b5771da16eb0adcac23880
lihy <953729298@qq.com>
2019-05-06 16:46:37 +0800

    配置修改

--- src/main/resources/static/ueditor/ueditor.config.js
+++ src/main/resources/static/ueditor/ueditor.config.js
@@ -21,6 +21,8 @@
      */
     var URL = window.UEDITOR_HOME_URL || getUEBasePath();
 
+    var SERVER_CONTEXT_PATH="";
+
     /**
      * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
      */
@@ -30,7 +32,7 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: "/ueditor.do"
+        , serverUrl: SERVER_CONTEXT_PATH + "/ueditor.do"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
